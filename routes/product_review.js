3ca93577ecabb4fca8158d9a8faeee3644b322a6
goldspide <raoulbonsso@gmail.com>
2025-01-05 00:09:59 +0100

    rating get methode implementation done

--- routes/product_review.js
+++ routes/product_review.js
@@ -19,4 +19,16 @@ productReviewRouter.post('/api/product-review', async(req,res)=>{
 })
 
 
+productReviewRouter.get('/api/review',async(req,res) => {
+    
+    try {
+    const reviews = await ProductReview.find();
+    return res.status(200).json(reviews);
+    } catch (error) {
+        return res.status(500).json({'error': error.message});
+        
+    }
+
+});
+
 module.exports = productReviewRouter;
